794095a106fef71452a5cc88f0adfb2eefbc71e1
Георгий <lrolfg@gmail.com>
2020-02-24 18:36:56 +0300

    Попап и разделение шапки и лида

---
+++ build/js/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var callbackButton = document.querySelector('.button--callback');
+var popupCallback = document.querySelector('.popup');
+var popupCloseHandler = document.querySelector('.feedback__close');
+var feedbackForm = document.querySelector('.feedback.feedback--popup');
+
+var isEscEvent = function (evt, action) {
+  if (evt.keyCode === 27) {
+    action();
+  }
+};
+
+var pressEscHandler = function (evt) {
+  isEscEvent(evt, closeCallback);
+};
+
+var openPopup = function () {
+  popupCallback.classList.remove('hidden');
+};
+
+var closePopup = function () {
+  popupCallback.classList.add('hidden');
+};
+
+var clickOutside = function (e) {
+  if (!feedbackForm.contains(e.target)) {
+    closePopup();
+  }
+};
+
+var openCallback = function () {
+  openPopup(popupCallback);
+  popupCloseHandler.addEventListener('click', closePopup);
+  popupCallback.addEventListener('click', clickOutside);
+  document.addEventListener('keydown', pressEscHandler);
+};
+
+var closeCallback = function () {
+  closePopup(popupCallback);
+  popupCloseHandler.removeEventListener('click', closePopup);
+  popupCallback.removeEventListener('click', clickOutside);
+  document.removeEventListener('keydown', pressEscHandler);
+};
+
+
+callbackButton.addEventListener('click', openCallback);
