409beef7b86a1ac02f4e2ba7c3c9842711492e95
Георгий <lrolfg@gmail.com>
2020-02-25 16:27:50 +0300

    Все переменные из всплывающего окна хранятся в localstorage

--- source/js/main.js
+++ source/js/main.js
@@ -7,10 +7,20 @@ var feedbackPhone = document.querySelector('input[name="phone"]');
 var popupPhone = document.querySelector('input[name="popup-phone"]');
 var feedbackPopup = document.querySelector('.feedback__wrapper.feedback__wrapper--popup');
 
+var nameValue = document.querySelector('#popup-name');
+var phoneValue = document.querySelector('#popup-phone');
+var textValue = document.querySelector('#popup-text');
+
 var isEscEvent = function (evt, action) {
   if (evt.keyCode === 27) {
     action();
   }
+};
+
+var saveDate = function () {
+  localStorage.setItem('name', nameValue.value);
+  localStorage.setItem('phone', phoneValue.value);
+  localStorage.setItem('text', textValue.value);
 };
 
 var pressEscHandler = function (evt) {
@@ -33,6 +43,7 @@ var outsideClickHandler = function (event) {
 
 var openCallback = function () {
   openPopup(popupCallback);
+  document.addEventListener('change', saveDate);
   popupCallback.addEventListener('click', outsideClickHandler);
   popupCloseHandler.addEventListener('click', closePopup);
   document.addEventListener('keydown', pressEscHandler);
@@ -40,6 +51,8 @@ var openCallback = function () {
 
 var closeCallback = function () {
   closePopup(popupCallback);
+  document.removeEventListener('input', saveDate);
+  popupCallback.removeEventListener('click', outsideClickHandler);
   popupCloseHandler.removeEventListener('click', closePopup);
   document.removeEventListener('keydown', pressEscHandler);
 };
@@ -49,6 +62,6 @@ var mask = IMask(feedbackPhone, {
   mask: '+{7}(000)000-00-00'
 });
 
-var mask2 = IMask(popupPhone, {
+var mask = IMask(popupPhone, {
   mask: '+{7}(000)000-00-00'
 });
