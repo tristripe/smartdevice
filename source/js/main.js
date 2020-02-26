61a78731818fb483572d77d4e4f9b0ae843c412f
Георгий <lrolfg@gmail.com>
2020-02-26 19:23:10 +0300

    IIFE и шрифты woff2

--- source/js/main.js
+++ source/js/main.js
@@ -1,72 +1,74 @@
 'use strict';
 
-var ESC = 27;
+(function () {
+  var ESC = 27;
 
-var callbackButton = document.querySelector('.button--callback');
-var popupCallback = document.querySelector('.popup');
-var popupCloseHandler = document.querySelector('.feedback__close');
-var feedbackPhone = document.querySelector('input[name="phone"]');
-var popupPhone = document.querySelector('input[name="popup-phone"]');
-var feedbackPopup = document.querySelector('.feedback__wrapper.feedback__wrapper--popup');
-var body = document.querySelector('body');
+  var callbackButton = document.querySelector('.button--callback');
+  var popupCallback = document.querySelector('.popup');
+  var popupCloseHandler = document.querySelector('.feedback__close');
+  var feedbackPhone = document.querySelector('input[name="phone"]');
+  var popupPhone = document.querySelector('input[name="popup-phone"]');
+  var feedbackPopup = document.querySelector('.feedback__wrapper.feedback__wrapper--popup');
+  var body = document.querySelector('body');
 
-var nameValue = document.querySelector('#popup-name');
-var phoneValue = document.querySelector('#popup-phone');
-var textValue = document.querySelector('#popup-text');
+  var nameValue = document.querySelector('#popup-name');
+  var phoneValue = document.querySelector('#popup-phone');
+  var textValue = document.querySelector('#popup-text');
 
-var isEscEvent = function (evt, action) {
-  if (evt.keyCode === ESC) {
-    action();
-  }
-};
+  var isEscEvent = function (evt, action) {
+    if (evt.keyCode === ESC) {
+      action();
+    }
+  };
 
-var saveDate = function () {
-  localStorage.setItem('name', nameValue.value);
-  localStorage.setItem('phone', phoneValue.value);
-  localStorage.setItem('text', textValue.value);
-};
+  var saveDate = function () {
+    localStorage.setItem('name', nameValue.value);
+    localStorage.setItem('phone', phoneValue.value);
+    localStorage.setItem('text', textValue.value);
+  };
 
-var pressEscHandler = function (evt) {
-  isEscEvent(evt, closeCallback);
-};
+  var pressEscHandler = function (evt) {
+    isEscEvent(evt, closeCallback);
+  };
 
-var openPopup = function () {
-  popupCallback.classList.remove('hidden');
-};
+  var openPopup = function () {
+    popupCallback.classList.remove('hidden');
+  };
 
-var closePopup = function () {
-  popupCallback.classList.add('hidden');
-};
+  var closePopup = function () {
+    popupCallback.classList.add('hidden');
+  };
 
-var outsideClickHandler = function (event) {
-  if (!feedbackPopup.contains(event.target)) {
-    closeCallback();
-  }
-};
+  var outsideClickHandler = function (event) {
+    if (!feedbackPopup.contains(event.target)) {
+      closeCallback();
+    }
+  };
 
-var openCallback = function () {
-  openPopup(popupCallback);
-  document.addEventListener('change', saveDate);
-  popupCallback.addEventListener('click', outsideClickHandler);
-  popupCloseHandler.addEventListener('click', closePopup);
-  document.addEventListener('keydown', pressEscHandler);
-  body.classList.add('scroll-hidden');
-};
+  var openCallback = function () {
+    openPopup(popupCallback);
+    document.addEventListener('change', saveDate);
+    popupCallback.addEventListener('click', outsideClickHandler);
+    popupCloseHandler.addEventListener('click', closePopup);
+    document.addEventListener('keydown', pressEscHandler);
+    body.classList.add('scroll-hidden');
+  };
 
-var closeCallback = function () {
-  closePopup(popupCallback);
-  document.removeEventListener('input', saveDate);
-  popupCallback.removeEventListener('click', outsideClickHandler);
-  popupCloseHandler.removeEventListener('click', closePopup);
-  document.removeEventListener('keydown', pressEscHandler);
-  body.classList.remove('scroll-hidden');
-};
-callbackButton.addEventListener('click', openCallback);
+  var closeCallback = function () {
+    closePopup(popupCallback);
+    document.removeEventListener('input', saveDate);
+    popupCallback.removeEventListener('click', outsideClickHandler);
+    popupCloseHandler.removeEventListener('click', closePopup);
+    document.removeEventListener('keydown', pressEscHandler);
+    body.classList.remove('scroll-hidden');
+  };
+  callbackButton.addEventListener('click', openCallback);
 
-var mask = IMask(feedbackPhone, {
-  mask: '+{7}(000)000-00-00'
-});
+  var mask = IMask(feedbackPhone, {
+    mask: '+{7}(000)000-00-00'
+  });
 
-var mask = IMask(popupPhone, {
-  mask: '+{7}(000)000-00-00'
-});
+  var mask = IMask(popupPhone, {
+    mask: '+{7}(000)000-00-00'
+  });
+})();
